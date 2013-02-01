ca8f381d407cb0204df567ba294598ea768751b1
mzwygart <mathieu@zwygart.net>
2013-02-01 10:57:33 +0100

    beautify js

--- js/jquery.ongletiseur.js
+++ js/jquery.ongletiseur.js
@@ -10,32 +10,31 @@ Authors: Mathieu Zwygart & Charlie the unicorn
 */
 
 (function($) {
-    $.fn.ongletiseur = function(options) {
-			var settings = $.extend({ activeClass: 'active' }, options);
-			
-			var tabsMenu = $(this).find('.tabs-menu'),
-			tabs = $(this).find('.tab'),
-			switchTab = function(tab){
-				tabs.hide();
-				$(tab).toggle();
-				tabsMenu.find('li').removeClass(settings.activeClass);
-				tabsMenu.find('a').each(function(){
-					if( $(this).attr('href') == tab){
-						$(this).parent().addClass(settings.activeClass);
-					}
-				})
-			}
-			
-			if(location.hash.length <= 0){
-				switchTab(tabsMenu.find('a').first().attr('href'));
-			}else{
-				switchTab(location.hash)
-			}
-			
-			$(window).hashchange(function(){
-				switchTab(location.hash)
-			});
-			
-			return this;	
-    };
+	$.fn.ongletiseur = function(options) {
+		var settings = $.extend({ activeClass: 'active' }, options),
+				tabsMenu = $(this).find('.tabs-menu'),
+				tabs = $(this).find('.tab'),
+				switchTab = function(tab){
+					tabs.hide();
+					$(tab).toggle();
+					tabsMenu.find('li').removeClass(settings.activeClass);
+					tabsMenu.find('a').each(function(){
+						if( $(this).attr('href') == tab){
+							$(this).parent().addClass(settings.activeClass);
+						}
+					})
+				};
+		
+		if (location.hash.length <= 0){
+			switchTab(tabsMenu.find('a').first().attr('href'));
+		} else {
+			switchTab(location.hash);
+		}
+		
+		$(window).hashchange(function(){
+			switchTab(location.hash);
+		});
+		
+		return this;	
+	};
 })(jQuery);
